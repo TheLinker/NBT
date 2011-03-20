5190fb4c342312209ba73c47e7102752d483a8d8
Brooks Boyd <boydb@midnightdesign.ws>
2011-03-20 16:53:52 -0500

    More "Player" sub-tags

--- generate_world.py
+++ generate_world.py
@@ -109,11 +109,44 @@ player = TAG_Compound()
 player.name = "Player"
 player.tags.extend([
 	TAG_Int(name="Score", value=0),
-	TAG_Int(name="Dimension", value=0)
+	TAG_Int(name="Dimension", value=0),
+	TAG_Short(name="Air", value=300),
+	TAG_Short(name="AttackTime", value=0),
+	TAG_Short(name="DeathTime", value=0),
+	TAG_Short(name="Fire", value=-20),
+	TAG_Short(name="Health", value=20),
+	TAG_Short(name="HurtTime", value=0),
+	TAG_Float(name="FallDistance", value=0),
+	TAG_Byte(name="OnGround", value=1),
+	TAG_Byte(name="Sleeping", value=0),
+	TAG_Short(name="SleepTimer", value=0)
 ])
 
 inventory = TAG_List(name="Inventory", type=TAG_Compound)
 player.tags.append(inventory)
+
+motion = TAG_List(name="Motion", type=TAG_Double)
+motion.tags.extend([
+	TAG_Double(value=0.0),
+	TAG_Double(value=0.0),
+	TAG_Double(value=0.0)
+])
+player.tags.append(motion)
+
+position = TAG_List(name="Pos", type=TAG_Double)
+position.tags.extend([
+	TAG_Double(value=spawn_x+0.5),
+	TAG_Double(value=2.8),
+	TAG_Double(value=spawn_z+0.5)
+])
+player.tags.append(position)
+
+rotation = TAG_List(name="Rotation", type=TAG_Float)
+rotation.tags.extend([
+	TAG_Float(value=0.0),
+	TAG_Float(value=0.0)
+])
+player.tags.append(rotation)
 
 data.tags.append(player)
 level.tags.append(data)
